b6499ee020f7c80b4de1b7caae700e7ce7ae6e1e
vittoriomaisano <108636033+vittoriomaisano@users.noreply.github.com>
2023-07-28 04:12:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,17 +25,16 @@ class ZoraBridger:
             value = eth_amount
 
         current_gas_price = self.web3_eth.eth.gasPrice
-        max_fee_per_gas = current_gas_price + self.web3_eth.toWei(10, "gwei")
-        max_priority_fee_per_gas = self.web3_eth.toWei(0.1, "gwei") 
+        max_fee_per_gas = current_gas_price + self.web3_eth.toWei(8, "gwei")
+        max_priority_fee_per_gas = self.web3_eth.toWei(0.1, "gwei")
 
-        #depositTransaction
-        data = self.bridge_contract.functions.depositTransaction(
-            self.web3_eth.toChecksumAddress(account.address),  # _to
-            self.web3_eth.toWei(value, "ether"),  # _value
-            100000,  # _gasLimit
-            False,   # _isCreation
-            b"",     # _data
-        ).buildTransaction({
+        gas_estimate = self.bridge_contract.functions.depositTransaction(
+            self.web3_eth.toChecksumAddress(account.address),
+            self.web3_eth.toWei(value, "ether"),
+            100000,
+            False,
+            b"",
+        ).estimateGas({
             "chainId": 1,
             "maxFeePerGas": max_fee_per_gas,
             "maxPriorityFeePerGas": max_priority_fee_per_gas,
@@ -43,7 +42,28 @@ class ZoraBridger:
             "value": self.web3_eth.toWei(value, "ether"),
         })
 
-        return account, data, eth_amount
+        gas_margin_percentage = 0.37
+        gas_margin = int(gas_estimate * gas_margin_percentage)
+
+        gas_limit = gas_estimate + gas_margin 
+
+        # depositTransaction
+        transaction = self.bridge_contract.functions.depositTransaction(
+            self.web3_eth.toChecksumAddress(account.address),
+            self.web3_eth.toWei(value, "ether"),
+            100000,
+            False,
+            b"",
+        ).buildTransaction({
+            "chainId": 1,
+            "maxFeePerGas": max_fee_per_gas,
+            "maxPriorityFeePerGas": max_priority_fee_per_gas,
+            "nonce": self.web3_eth.eth.getTransactionCount(account.address),
+            "value": self.web3_eth.toWei(value, "ether"),
+            "gas": gas_limit,
+        })
+
+        return account, transaction, eth_amount
 
     def send_transaction(self, account, data, wallet_index, eth_amount):
         signed_transaction = account.signTransaction(data)
@@ -66,18 +86,26 @@ class ZoraBridger:
 RESULTS_DIR = "results"
 
 
-def record_failed_to_csv(private_key, address, eth_amount):
+def write_failed_to_csv(wallet_index, private_key, address, eth_amount):
     results_path = os.path.join(RESULTS_DIR, "failed.csv")
     with open(results_path, "a", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow([private_key, address, eth_amount])
+
+        if file.tell() == 0:
+            writer.writerow(['wallet_index', 'private_key', 'address', 'eth_amount'])
+
+        writer.writerow([wallet_index, private_key, address, eth_amount])
 
 
-def record_success_to_csv(private_key, address, eth_amount):
+def write_success_to_csv(wallet_index, private_key, address, eth_amount):
     results_path = os.path.join(RESULTS_DIR, "success.csv")
     with open(results_path, "a", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow([private_key, address, eth_amount])
+
+        if file.tell() == 0:
+            writer.writerow(['wallet_index', 'private_key', 'address', 'eth_amount'])
+
+        writer.writerow([wallet_index, private_key, address, eth_amount])
 
 
 def main():
@@ -92,13 +120,15 @@ def main():
         cprint("Отсутствуют приватные ключи. Завершаю работу.", "red")
         return
 
-    if shuffle:
-        random.shuffle(private_keys)
-
     if not os.path.exists(RESULTS_DIR):
         os.mkdir(RESULTS_DIR)
 
-    for i, private_key in enumerate(private_keys, start=1):
+    numbered_private_keys = list(enumerate(private_keys, start=1))
+    if shuffle:
+        random.shuffle(numbered_private_keys)
+    private_keys = [pk for _, pk in numbered_private_keys]
+
+    for wallet_index, private_key in enumerate(private_keys, start=1):
 
         account, tx_data, eth_amount = bridger.create_transaction_data(private_key, zora_contract_address)
 
@@ -111,16 +141,15 @@ def main():
                 print(f"Достигнуто максимальное количество попыток({max_gas_check_attempts}). Завершаю работу...")
                 return
 
-            print(f"Попытки {gas_check_attempts}: Gwei ({bridger.web3_eth.fromWei(bridger.web3_eth.eth.gasPrice, 'gwei')}) Выше чем максимально допустимый ({max_gwei} gwei). Сплю...")
+            print(f"Попытка {gas_check_attempts}: Gwei ({bridger.web3_eth.fromWei(bridger.web3_eth.eth.gasPrice, 'gwei')}) Выше чем максимально допустимый ({max_gwei} gwei). Сплю 10 секунд...")
             time.sleep(10)
 
-        receipt = bridger.send_transaction(account, tx_data, i, eth_amount)
+        receipt = bridger.send_transaction(account, tx_data, wallet_index, eth_amount)
         if receipt is None:
-            record_failed_to_csv(private_key, account.address, eth_amount)
+            write_failed_to_csv(wallet_index, private_key, account.address, eth_amount)
             continue
 
-
-        record_success_to_csv(private_key, account.address, eth_amount)
+        write_success_to_csv(wallet_index, private_key, account.address, eth_amount)
 
         delay = random.randint(min_delay, max_delay)
         time.sleep(delay)
